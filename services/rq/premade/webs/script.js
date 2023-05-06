2ebfba4a9386401ae0819bee59c788846b22ce1b
zexymezexycoco1iszexy <119825964+zexymezexycoco1iszexy@users.noreply.github.com>
2023-05-06 14:18:13 -0700

    Add files via upload

---
+++ services/rq/premade/webs/script.js
@@ -0,0 +1,9 @@
+function redirectToPage1() {
+  window.location.href = "https://buy.stripe.com/8wM00Odbm19gfdu3ce";
+}
+function redirectToPage2() {
+  window.location.href = "https://buy.stripe.com/5kAbJwc7i8BIc1i7sv";
+}
+function redirectToPage3() {
+  window.location.href = "https://buy.stripe.com/5kA4h47R2cRY8P69AE";
+}
